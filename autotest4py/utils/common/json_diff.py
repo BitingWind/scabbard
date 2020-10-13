d5d55065dc9ca74e10351dcb5d1139cae1183c83
pep.zhang <pepzhang@tencent.com>
2020-10-13 19:29:29 +0800

    add dict remove field

--- autotest4py/utils/common/json_diff.py
+++ autotest4py/utils/common/json_diff.py
@@ -1,5 +1,37 @@
 # encoding: utf-8
 
+
+# 去除dict中的指定field，暴力方式，非屏蔽直接抹掉
+def case_list(l, fileds):
+    """
+    如果是list,得遍历里面看是不是dict来判断需不需要剔除
+    """
+    for i in l:
+        if isinstance(i, dict):
+            case_dict(i, fileds)
+        if isinstance(i, list):
+            case_list(i, fileds)
+
+
+def case_dict(m, fileds):
+    """
+    如果是一个dict，递归遍历
+    """
+    ks = []
+    for k, v in m.items():
+        if k in fileds:
+            # 需要剔除的，先记下来，遍历完了之后做剔除
+            ks.append(k)
+        else:
+            if isinstance(v, dict):
+                case_dict(v, fileds)
+            if isinstance(v, list):
+                case_list(v, fileds)
+
+    for i in ks:
+        del m[i]
+
+
 # 原文链接：https://blog.csdn.net/u010080628/java/article/details/79538420
 class JsonCompare:
     def __init__(self, expect_data, real_data, is_debug=False):
@@ -60,11 +92,12 @@ class JsonCompare:
         for i in self.frame_compare_result:
             print(i)
 
+
 if __name__ == '__main__':
     master_dict = {"model_type":"event","metadata_id":"mid_api","table_conf":{"dims":[{"field_key":"2930","source_table":None,"field_name":"中间层分析查询结果","field_type":"string","dim_type":"custom"}],"indexs":[{"field_key":"count_all:mid_api","field_name":"中间层埋点[总次数]","unit":"","accuracy":"","is_percent":False}],"precise_remove_repetition":False,"user_groups":[]},"sample_ratio":1,"data_resource_id":2459,"uin_conf":{"field_key":"id","uin_type":"business_account"},"filters":{"logic":"and","wheres":[{"operator":"-is","value":"web_api,open_api","type":None,"field_key":"api_request_type","metadata_id":"mid_api","source_table":None,"custom_field_key":None},{"operator":"-is","value":"release_in","type":None,"field_key":"environment","metadata_id":"mid_api","source_table":None,"custom_field_key":None}]},"chart_type":"bar","chart_conf":{"limit":"1000","chart_show_type":None,"merge_date":None,"query_date":{"localDateTimes":None,"date_range":"24","date_type":"hour","date_format":"1","date_group":"","relative_type":0},"query_date_compare":None,"order_by":[{"field_key":"2930","field_type":"dim","source_table":None,"rule":"desc"}],"show_group":[],"index_filter":[],"secondary_y_axis":[]}}
     candidate_dict = {"uin_conf": {"field_key": "id", "uin_type": "business_account"}, "data_resource_id": 2459, "table_conf": {"dims": [{"field_key": "2930", "source_table": None, "field_name": "\u4e2d\u95f4\u5c42\u5206\u6790\u67e5\u8be2\u7ed3\u679c", "dim_type": "custom", "field_type": "string"}], "user_groups": [], "precise_remove_repetition": False, "indexs": [{"field_key": "count_all:mid_api", "is_percent": False, "field_name": "\u4e2d\u95f4\u5c42\u57cb\u70b9[\u603b\u6b21\u6570]", "unit": "", "accuracy": ""}]}, "chart_type": "pie", "filters": {"wheres": [{"custom_field_key": None, "source_table": None, "value": "web_api,open_api", "operator": "-is", "field_key": "api_request_type", "metadata_id": "mid_api", "type": None}, {"custom_field_key": None, "source_table": None, "value": "release_in", "operator": "-is", "field_key": "environment", "metadata_id": "mid_api", "type": None}], "logic": "and"}, "chart_conf": {"order_by": [{"field_key": "count_all:mid_api", "source_table": None, "rule": "desc", "field_type": "index"}], "index_filter": [], "query_date_compare": None, "secondary_y_axis": [], "show_group": [], "limit": "1000", "query_date": {"date_format": "1", "relative_type": 0, "date_type": "hour", "date_range": "24", "date_group": "", "localDateTimes": None}, "chart_show_type": None, "merge_date": None}, "model_type": "event", "metadata_id": "page_visit", "sample_ratio": 1.0}
     json_compare_result = JsonCompare(master_dict, candidate_dict)
     if not json_compare_result.is_full_consistent():
-        print u'-----------------------------------【DIFF FAIL】不一致结果: -----------------------------------'
+        print(u'-----------------------------------【DIFF FAIL】不一致结果: -----------------------------------')
         # Json 错误结果输出
         json_compare_result.print_diff()
